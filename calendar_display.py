b228e3749b043df9477787b66c3024de14ecf347
georgescutelnicu <george.v.scutelnicu@gmail.com>
2025-03-27 19:30:03 +0200

    update: add task status colors and adjust spacing

--- calendar_display.py
+++ calendar_display.py
@@ -1,6 +1,6 @@
 import calendar
 import datetime
-from task_manager import handle_task_management
+from task_manager import get_tasks_for_day, handle_task_management
 
 
 def display_calendar(year, month):
@@ -12,12 +12,20 @@ def display_calendar(year, month):
     for week in cal:
         line = ""
         for day in week:
+            tasks = get_tasks_for_day(year, month, day)
             if day == 0:
                 line += "   "
             elif day == today.day and month == today.month and year == today.year:
                 line += f" \033[1;1;34m{day:2d}\033[0m"
+            elif tasks:
+                pending_tasks = sum(1 for task in tasks if task["status"] == "pending")
+                if pending_tasks == 0:
+                    line += f" \033[1;32m{day:2d}\033[0m"
+                else:
+                    line += f" \033[1;31m{day:2d}\033[0m"
             else:
                 line += f" {day:2d}"
+
         calendar_lines.append(line)
 
     legend_lines = [
@@ -25,14 +33,14 @@ def display_calendar(year, month):
         "",
         "\033[1;34mBlue\033[0m - Current Day",
         "\033[0mWhite - Free Day",
-        "\033[1;32mGreen\033[0m - Task Completed",
-        "\033[1;31mRed\033[0m - Task Pending"
+        "\033[1;32mGreen\033[0m - Tasks Completed",
+        "\033[1;31mRed\033[0m - Tasks Pending"
     ]
 
     print()
     for i in range(len(calendar_lines)):
         legend_line = legend_lines[i] if i < len(legend_lines) else ""
-        print(f"{calendar_lines[i].ljust(30)} {legend_line}")
+        print(f"{calendar_lines[i]}      {legend_line}")
 
     print(f"\n\n [\033[1;97m1-{calendar.monthrange(year, month)[1]}\033[0m] Check Day  "
           f"[\033[1;97mN\033[0m] Next Month [\033[1;97mP\033[0m] Previous Month  [\033[1;97mQ\033[0m] Quit\n")
